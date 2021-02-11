64c143bc296b23a2621cef80ee19b2991e5afe86
MeryemGh <meryemgh76@gmail.com>
2021-02-11 14:14:53 +0100

    Paper

---
+++ App.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import { Appbar, Searchbar } from 'react-native-paper';
+import { ScrollView, StyleSheet, View ,Text,Image} from 'react-native';
+
+  export default class App extends Component {
+render(){
+  return(
+    
+  <View style={{ flex:1 ,justifyContent:'space-between',backgroundColor: "#E0E0E0"}}>
+ <Appbar style={styles.bottom}>
+          <Appbar.Content title="LoDeep"  />
+          <Appbar.Action icon="home"  size={17} onPress={() => console.log('HOME')} />
+
+          <Appbar.Action icon="plus" size={17} onPress={() => console.log('plus')} />
+          
+          <Appbar.Action icon="contacts"  size={17} onPress={() => console.log('HOME')} />
+          <Appbar.Action icon="chart-line"  size={17} onPress={() => console.log('HOME')} />
+
+          <Appbar.Action icon="notification-clear-all"  size={17} onPress={() => console.log('Pressed mail')} />
+          <Appbar.Action icon="cog"  size={17} onPress={() => console.log('cog pressed')} />
+          <Appbar.Action icon="logout"  size={17} onPress={() => console.log('logout pressed')} />
+  </Appbar>
+  <View style={{flex:1 ,marginTop:16}}> 
+    <Searchbar></Searchbar>
+  </View>
+
+  </View> 
+ );
+
+  }}
+const styles = StyleSheet.create({
+  bottom: {
+    position: 'relative',
+    left: 0,
+    right: 0,
+    bottom: 0,
+    backgroundColor:"#FFFFFF"  },
+    container: {
+
+    }
+
+});
